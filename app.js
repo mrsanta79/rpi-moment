59c6c16c91c9e55dee5094b8e2c2440f3b529def
Santanu Biswas <dev.sbiswas7991@gmail.com>
2021-03-16 12:19:12 +0530

    feat: weather api, page loader

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 
 // Environment Variables
-const APP_PORT = 3000
+const APP_PORT = 3000;
+const WEATHER_API_KEY = '4d7a47b8d55bac8c2e78904bee39da77';
+const LOCATION = {
+    CITY: 'Habra',
+    STATE_CODE: 'WB',
+    COUNTRY_CODE: 'IN'
+};
 
 // Initialize app
 const app = express();
@@ -20,7 +26,8 @@ app.use('/assets', express.static(path.join(__dirname, '/assets')));
 // Routes
 app.get('/', (req, res) => {
     const data = {
-        test: 'Hello'
+        WEATHER_API_KEY,
+        LOCATION
     };
 
     res.status(200).render('index', data);
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,13 @@ let app = new Vue({
     data: {
         currentTime: '-- : -- : --',
         currentDate: '-',
+        currentTemperature: '-',
+        currentTemperatureIcon: '',
+        currentTemperatureType: '-',
     },
     created() {
         this.getTodayInfo();
+        this.getCurrentTemperature();
     },
     methods: {
         getTodayInfo: function() {
@@ -18,6 +22,25 @@ let app = new Vue({
                 this.currentDate = day + ', ' + date;
                 this.currentTime = time;
             }, 1000);
+        },
+        getCurrentTemperature: function() {
+            const API_KEY = document.querySelector('meta[name=weather-api-key]').getAttribute('content');
+            const CITY = document.querySelector('meta[name=location-city]').getAttribute('content');
+            const STATE = document.querySelector('meta[name=location-state]').getAttribute('content');
+            const COUNTRY = document.querySelector('meta[name=location-country]').getAttribute('content');
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${CITY},${STATE},${COUNTRY}&appid=${API_KEY}`;
+            const weatherIconUrl = `https://openweathermap.org/img/wn/`;
+
+            // setInterval(() => {
+            fetch(weatherUrl).then(data => data.json())
+                .then(response => {
+                    console.log(response);
+                    this.currentTemperature = response.main.temp - 273.15;
+                    this.currentTemperatureIcon = weatherIconUrl + response.weather[0].icon + '@2x.png';
+                    this.currentTemperatureType = response.weather[0].main;
+                })
+                // }, 60000);
         }
     }
 });
